f40cc91e3e2a8d24dcf5e2ab72ec6673412791c0
Denys <80397210+juntapowerrr@users.noreply.github.com>
2023-10-21 00:21:33 +0300

    first commit

---
+++ js/fixedheaderscroll.js
@@ -0,0 +1,61 @@
+//плавний скролл та автовідступ контента від хедера після скроллу
+(function($) {
+
+    $.fn.fixedHeaderScroll = function(options) {
+
+        var settings = $.extend({
+            'headerSelector': '.header', // You need to designate the header element in your website.
+            'offset': 0, // You can offset the scroll position.
+            'smooth': true, // You can choose if you use smooth scroll or not.
+            'smoothDuration': 300, // Scroll speed. (in ms)
+            'smoothEasing': 'swing', // Scroll easing.
+        }, options);
+
+        function fixScroll(targetSlector) {
+            if (settings.headerSelector == false) {
+                var h = 0;
+            } else {
+                var headerEl = $(settings.headerSelector);
+                var h = headerEl.outerHeight();
+            }
+
+            var targetEl = $(targetSlector);
+            if( targetEl.length < 1) {
+                return false;
+            }
+            var t = targetEl.offset().top;
+            var s = t - h + settings.offset;
+
+            if (settings.smooth) {
+                $("html, body").stop(true, false).animate({
+                    scrollTop: s
+                }, settings.smoothDuration, settings.smoothEasing);
+            } else {
+                $("html, body").scrollTop(s);
+            }
+            return $(this);
+        }
+
+        this.unbind( "click.fixedHeaderScroll");
+        this.bind( "click.fixedHeaderScroll", function (){
+            fixScroll( $(this).attr("href"));
+        });
+
+        $(window).on('load', function() {
+            if (location.hash) {
+                fixScroll(location.hash);
+                return false;
+            }
+        });
+
+    };
+})(jQuery);
+
+//налаштування переходу по якорю, щоб решітка не відображалася в адресному рядку після переходу:
+$('a[href^="#"]').bind("click", function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+    }, 300);
+    e.preventDefault();
+});
---
+++ js/header.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.header__link').fixedHeaderScroll( {
+        headerSelector: 'header',
+        offset: 0,
+        offset: $('.header').offsetHeight,
+        smooth: true,
+        smoothDuration: 500,
+        smoothEasing: 'swing'
+    });
+
+    $('.header__burger, .header__link').click(function(event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+    $(window).on('scroll', function() {
+        $('.container').css({
+            opacity: 1,
+        })
+    });
+});
