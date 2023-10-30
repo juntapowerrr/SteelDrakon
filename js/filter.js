ad44b4aa081208101591c7eb56d25633617bd221
Denys <80397210+juntapowerrr@users.noreply.github.com>
2023-10-30 21:20:56 +0200

    fix filter + other fixes

--- js/filter.js
+++ js/filter.js
@@ -1,25 +1,39 @@
-$(document).on("click","span.all", function(){
-    $("div.item").show().effect("fade","fast");
-})
+function updateButtonStyles(clickedButton) {
+  // Сбросить стили для всех кнопок
+  $(".buttons span").css({
+    "background-color": "",
+    "color": ""
+  });
 
-$("span.landscaping").on("click", function(){
+  // Установить стили для активной кнопки
+  clickedButton.css({
+    "background-color": "#F7CA3A",
+    "color": "#000000"
+  });
+}
+
+$(document).ready(function () {
+  // Обработчики событий для кнопок фильтрации
+  $(".buttons span").on("click", function () {
+    updateButtonStyles($(this));
+    var category = $(this).attr("class");
+    if (category === "all") {
+      $("div.item").fadeIn("fast");
+    } else {
       $("div.item").hide();
-      $("div.landscaping").show().effect("fade","fast");
-})
+      $("div." + category).fadeIn("fast");
+    }
+  });
 
-$("span.metal").on("click", function(){
-      $("div.item").hide();
-      $("div.metal").show().effect("fade","fast");
-})
-// $("span.temp").on("click", function(){
-//       $("div.item").hide();
-//       $("div.temp").show().effect("fade","fast");
-// })
+  // Активация первой кнопки по умолчанию
+  $(".buttons span.all").click();
+});
 
-/*
-.slideUp(); - скрыть
-.slideDown(); - показать
-*/
+$(document).on("click", "span.all", function () {
+  updateButtonStyles($(this));
+  $("div.item").fadeIn("fast");
+});
+
 const element = document.querySelector(".buttons");
 
 element.addEventListener('wheel', (event) => {
